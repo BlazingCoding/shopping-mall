6186bdbf4ae7326c5d0db12c804168735e38c8d8
BlazingCoding <h91oon@gmail.com>
2020-11-06 09:21:19 +0900

    paypal payment feature done

--- client/src/components/utils/Paypal.js
+++ client/src/components/utils/Paypal.js
@@ -7,6 +7,7 @@ export default class Paypal extends React.Component {
             // Congratulation, it came here means everything's fine!
             console.log("The payment was succeeded!", payment);
             // You can bind the "payment" object's value to your state or props or whatever here, please see below for sample returned data
+            this.props.onSuccess(payment)
         }
 
         const onCancel = (data) => {
@@ -24,11 +25,11 @@ export default class Paypal extends React.Component {
 
         let env = 'sandbox'; // you can set here to 'production' for production
         let currency = 'USD'; // or you can set this value from your props or state
-        let total = 1; // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
+        let total = this.props.total; // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
         // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
 
         const client = {
-            sandbox:    'YOUR-SANDBOX-APP-ID',
+            sandbox:    'Ad5_ryJyGTXS89Smc_W48UyWlrSDC5zj-D9fxWklyUOlDsNOK15FAAK5rnFjmXU3z9RYgUODk9XFvFEJ',
             production: 'YOUR-PRODUCTION-APP-ID',
         }
         // In order to get production's app-ID, you will have to send your app to Paypal for approval first
--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -1,8 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import { useDispatch } from "react-redux";
-import { getCartItems, removeCartItem } from "../../../_actions/user_actions"
+import { getCartItems, removeCartItem, onSuccessBuy } from "../../../_actions/user_actions"
 import UserCardBlock from "./Sections/UserCardBlock";
-import {Empty} from "antd";
+import {Empty, Result} from "antd";
 import Paypal from "../../utils/Paypal";
 
 
@@ -11,6 +11,7 @@ function CartPage(props) {
 
     const [Total, setTotal] = useState(0);
     const [ShowTotal, setShowTotal] = useState(false);
+    const [ShowSuccess, setShowSuccess] = useState(false);
 
     useEffect(() => {
         let cartItems=[]
@@ -47,6 +48,19 @@ function CartPage(props) {
             })
     }
 
+    const transactionSuccess = (data) => {
+        dispatch(onSuccessBuy({
+            paymentData: data,
+            cartDetail: props.user.cartDetail
+        }))
+            .then(response => {
+                if(response.payload.success) {
+                    setShowTotal(false)
+                    setShowSuccess(true)
+                }
+            })
+    }
+
     return (
         <div style={{ width: '85%', margin: '3rem auto' }}>
             <h1>My Cart</h1>
@@ -54,18 +68,28 @@ function CartPage(props) {
                 <UserCardBlock products={props.user.cartDetail} removeItem={removeFromCart}/>
             </div>
 
+
+
             {ShowTotal ?
                 <div style={{ marginTop: '3rem'}}>
                     <h2>Total Amount: ${Total}</h2>
                 </div>
-                :
-                <div>
-                    <br/>
-                    <Empty description={false}/>
-                </div>
+                : ShowSuccess ?
+                    <Result
+                        status="success"
+                        title="Successfully Purchased Items"
+                    />
+                    :
+                    <div>
+                        <br/>
+                        <Empty description={false}/>
+                    </div>
             }
-
-            <Paypal/>
+            {ShowTotal &&
+            <Paypal
+                total={Total}
+                onSuccess={transactionSuccess}
+            />}
 
         </div>
     );
